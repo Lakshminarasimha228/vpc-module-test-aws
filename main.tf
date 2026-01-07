2896e3170045de0740d6a960a1d6290005b88ea1
lakshmi <lakshminarasimha228@gmail.com>
2026-01-07 10:07:55 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     #source = "../terraform-aws-vpc"
-    source = "git::https://github.com/Lakshminarasimha228/vpc-module-test-aws.git?ref=main"
+    source = "git::https://github.com/Lakshminarasimha228/terraform-aws-vpc.git?ref=main"
     /* project = "roboshop"
     environment = "dev"
     public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"] */
